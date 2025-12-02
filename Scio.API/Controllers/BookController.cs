f09ec3eb79b36707285fb7245ec80a8baefe95ac
Tomas Kuttler <tomas.kuttler@gmail.com>
2025-12-02 07:34:09 +0100

    fixed warnings for nullable BorrowedBooksInfo, Removed duplicated model declaration.

--- Scio.API/Controllers/BookController.cs
+++ Scio.API/Controllers/BookController.cs
@@ -4,15 +4,6 @@ using Scio.API.Services;
 
 namespace Scio.API.Controllers
 {
-    public class BorrowedBookInfo
-    {
-        public Guid BookId { get; set; }
-        public string BookTitle { get; set; } = string.Empty;
-        public string BookAuthor { get; set; } = string.Empty;
-        public Guid BorrowRecordId { get; set; }
-        public string UserName { get; set; } = string.Empty;
-        public DateTime BorrowDate { get; set; }
-    }
 
     [ApiController]
     [Route("api/[controller]")]
